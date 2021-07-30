9d39ca0a0b12f52e950d7c076118436b3b66bf16
Ionut Motoi <ionut.m.motoi@gmail.com>
2021-07-30 11:32:15 +0200

    reachability.py added comment

---
+++ reachability.py
@@ -0,0 +1,7 @@
+def reachability_solver(arena, R):
+    """
+    Reachability solver
+    :param arena:
+    :param R: reachability set
+    :return: two tuples -> winning region and strategy for both players
+    """
