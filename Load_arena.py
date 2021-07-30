0819c6a6d776bfcc6a4757f06e85de1a30edc741
Leonardo Saraceni <saracenileonardo01@gmail.com>
2021-07-30 10:13:04 +0200

    Loading arena from file

---
+++ Load_arena.py
@@ -0,0 +1,26 @@
+from arena import Arena
+
+"""
+File used to load the arena, and print out the solution
+"""
+
+
+def load_arena(file):
+    with open(file, "r") as f:
+        arena = Arena()
+        for line in f:
+            split_line = line.split(" ")
+            node = int(split_line[0])
+            if split_line[1] == "0":
+                arena.add_node(node, 0)
+            else:
+                arena.add_node(node, 1)
+
+            for succ in split_line[3].split(","):
+                arena.add_successor(node, int(succ))
+                arena.add_predecessor(int(succ), node)
+
+    return arena
+
+
+
